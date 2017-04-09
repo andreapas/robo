1f0ae3dc250a9e22f13f80ac3a280687eea220d4
Andrea Pasquali <andrea_pasquali@hotmail.it>
2017-04-09 11:52:30 +0200

    Added minimum code

--- Robot/Robot/src/entities/ConnectionInfos.java
+++ Robot/Robot/src/entities/ConnectionInfos.java
@@ -4,6 +4,7 @@ public class ConnectionInfos {
 
 	private String ip;
 	public static int INPUT_PORT=4000;
+	public static int POSE_PORT=60000;
 	public ConnectionInfos(String ip) {
 		this.ip = ip;
 	}
