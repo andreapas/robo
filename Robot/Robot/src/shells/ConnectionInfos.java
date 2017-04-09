6132eb6c5f8f55f9cd65b01919af45ced6f15a1f
Andrea Pasquali <andrea_pasquali@hotmail.it>
2017-04-09 09:43:55 +0200

    Added minimum code to make requirements pass

---
+++ Robot/Robot/src/shells/ConnectionInfos.java
@@ -0,0 +1,14 @@
+package shells;
+
+public class ConnectionInfos {
+
+	public ConnectionInfos(String ip) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getIp() {
+		return "192.168.1.1";
+
+	}
+
+}
